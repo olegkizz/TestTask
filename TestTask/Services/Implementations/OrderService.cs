4407e7e7af56b4c74aa70904e067b415620f099d
Acer <Acer@DESKTOP-4C5MII3>
2024-02-29 04:36:12 +0300

    Добавьте файлы проекта.

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _context;
+        public OrderService(ApplicationDbContext context)
+        {
+            _context = context;
+
+        }
+        public async Task<Order> GetOrder()
+        {
+            return await _context.Orders.OrderByDescending(o => o.Price)
+                       .FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Order>> GetOrders()
+        {
+            return await _context.Orders.Where(o => o.Quantity > 10).ToListAsync();
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _context;
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+
+        }
+        public async Task<User> GetUser()
+        {
+            return  await _context.Users.OrderByDescending(u => u.Orders.Count)
+                        .FirstOrDefaultAsync();
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            return await _context.Users
+                .Where(u => u.Status == Enums.UserStatus.Inactive)
+                .ToListAsync();
+        }
+    }
+}
